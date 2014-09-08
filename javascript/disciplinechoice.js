535a19f4858e36c38cf1fdbf54dd3b81550f7362
Jad El-Houssami <jad.el-houssami@my.westminster.ac.uk>
2014-09-08 15:32:27 +0100

    Added code to empty cells when Discipline drop down value changes and added scenario for Road option

--- javascript/disciplinechoice.js
+++ javascript/disciplinechoice.js
@@ -3,22 +3,62 @@ $(function()
 	var discipline = $('#discipline');
 	var distanceLabel = $('#distancelabel');
 	var distanceInput = $('#distanceinput');
-	var newElement;
+	var newInput;
+	var newOptionValue;
+	var nonBreakingSpace = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
 	
-	//Change label and form elements when discipline drop-down value changes.
+	//Change label and form elements when discipline drop down value changes.
 	discipline.on('change', function()
 	{
+		//Always empty cells when the drop down value changes first.
+		distanceLabel.empty();
+		distanceInput.empty();
+
 		switch(discipline.val())
 		{
 			case "Track":
 				distanceLabel.text("Distance (metres)");
-				newElement = $("<input/>",
+				newInput = $("<input/>",
 				{
 					type: "text",
-					id: "disciplineinput"
+					id: "disciplineinput",
+					value: "tracktest"
 				});
-				distanceInput.append(newElement);
+				distanceInput.append(newInput);
 				break;	
+			case "Road":
+				distanceLabel.text("Distance");
+				newInput = $("<input/>",
+				{
+					type: "text",
+					id: "disciplineinput",
+					value: "roadtest"
+				});
+				distanceInput.append(newInput);
+				distanceInput.append(nonBreakingSpace);
+				
+				newDropDown = $("<select/>",
+				{
+					id: "unitofmeasure",
+				});
+
+				distanceInput.append(newDropDown);
+
+				newOptionValue = $("<option />",
+				{
+					value: "K",
+					text: "Kilometres"
+				});
+
+				newDropDown.append(newOptionValue);
+
+				newOptionValue = $("<option />",
+				{
+					value: "M",
+					text: "Miles"
+				});
+
+				newDropDown.append(newOptionValue);				
 		}
 	});
 });
