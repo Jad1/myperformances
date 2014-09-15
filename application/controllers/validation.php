97f61decf8f60736e0eb1ed20055b701581b6109
Jad El-Houssami <jad.el-houssami@my.westminster.ac.uk>
2014-09-15 22:16:42 +0100

    added validateDetails method

--- application/controllers/validation.php
+++ application/controllers/validation.php
@@ -12,7 +12,35 @@
 
 	public function validateDetails()
 	{
-		
+		$discipline = $this->input->post('discipline');
+		$disciplineInput = $this->input->post('disciplineinput');
+		$unitOfMeasure = '';
+		$hrs = $this->input->post('hrs');
+		$mins = $this->input->post('mins');
+		$secs = $this->input->post('secs');
+		$eventName = $this->input->post('eventname');
+		$eventLocation = $this->input->post('eventlocation');
+		$eventDay = intval($this->input->post('day'));
+		$eventMonth = intval($this->input->post('month'));
+		$eventYear = intval($this->input->post('year'));
+
+		//Value of $unitOfMeasure depends on value of $discipline
+		switch($discipline)
+		{
+			case "Road":
+			case "XC":
+				$unitOfMeasure = $this->input->post('unitofmeasure');
+				break;
+			//Do nothing otherwise.
+			case "Track":
+			default:
+				break;
+		}
+
+		//See comments above methods to check their functionality.
+		validateDates($eventDay, $eventMonth, $eventYear);
+		validateTextLengths();
+		validateTextInputs();
 	}
 
 
