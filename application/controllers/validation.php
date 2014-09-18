ece752c4d04e91867a134f46788e96ceee802dfb
Jad El-Houssami <jad.el-houssami@my.westminster.ac.uk>
2014-09-18 12:56:50 +0100

    Added scenario for 30 day months and comment for 31 day months

--- application/controllers/validation.php
+++ application/controllers/validation.php
@@ -78,13 +78,17 @@
 			case 8:
 			case 10:
 			case 12:
-				//Months with 31 days
+				//Months with 31 days. Do nothing as drop down only goes up to 31.
 				break;
 			case 4:
 			case 6:
 			case 9:
 			case 11:
 				//Months with 30 days
+				if($eventDay > 30)
+				{
+					return "Date is invalid (This month only has 30 days)";
+				}
 				break;
 			default:
 				break;			
